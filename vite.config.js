7dbce26dba253592e270f577fb1f43f68d4047a9
Amankwatia <brightamankwatia@gmail.com>
2025-11-12 00:55:23 +0000

    Update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       '@': resolve(__dirname, 'src')
     }
   },
-  base: '/presec/',
+  base: '/presec-berekum/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
